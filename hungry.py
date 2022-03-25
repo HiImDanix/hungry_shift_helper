9a118b49bc0868630a937ba6c0c7a455cf029b0e
Daniels Kanepe <danixkanepe@gmail.com>
2022-03-25 15:42:11 +0100

    Recurring timeslots are now saved to a file
    Recurring timeslots are saved to file. If it does not exist, a default timeslot that covers all times possible is created.
    If a timeslot-file is specified, an error is thrown if that file does not exist. Otherwise it loads the timeslots from it.

--- hungry.py
+++ hungry.py
@@ -1,5 +1,6 @@
 import argparse
 import json
+import pathlib
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 from typing import Set
@@ -20,9 +21,15 @@ if __name__ == "__main__":
     parser.add_argument("password", help="Your hungry.dk password", type=str)
     parser.add_argument("id", help="Your hungry.dk employee ID (see app -> my profile)", type=int)
     parser.add_argument("notify", help="Apprise notification URL")
+    parser.add_argument("-d", "--debug", help="Enable debug mode", action="store_true")
+    parser.add_argument("--timeslot-file", help="File to load timeslots from", type=pathlib.Path)
     parser.add_argument("-f", "--frequency", help="Executes the script every <seconds> (use CRON instead!)",
                         metavar="seconds", type=int)
     args = parser.parse_args()
+
+    # Check that timeslot-file exists, otherwise throw exception
+    if args.timeslot_file is not None and not args.timeslot_file.exists():
+        raise FileNotFoundError(f"Timeslot file {args.timeslot_file} does not exist")
 
     # Notifications
     appriseObj = apprise.Apprise()
@@ -32,30 +39,44 @@ if __name__ == "__main__":
     # Hungry
     hungry = HungryAPI(args.email, args.password, args.id)
 
-    # Create a recurring timeslot that covers all days of week, all hours, and all shift lengths
-    # days of week
-    days_of_week = [0, 1, 2, 3, 4, 5, 6]
-    # start and end time
-    start = datetime.strptime("00:00", "%H:%M")
-    end = datetime.strptime("23:59", "%H:%M")
-    # min shift length (minutes)
-    shift_length = 0
-    # create a recurring timeslot
-    timeslot = RecurringTimeslot(days_of_week, start.time(), end.time(), shift_length)
+    # load timeslots from file.
+    try:
+        with open("timeslots.json", "r") as f:
+            timeslots = [RecurringTimeslot.from_json(timeslot) for timeslot in json.load(f)]
+    except FileNotFoundError:
+        print("No timeslot file found. Creating new one.")
+        # Create a recurring timeslot that covers all days of week, all hours, and all shift lengths
+        # days of week
+        days_of_week = [0, 1, 2, 3, 4, 5, 6]
+        # start and end time
+        start = datetime.strptime("00:00", "%H:%M")
+        end = datetime.strptime("23:59", "%H:%M")
+        # min shift length (minutes)
+        shift_length = 0
+        # create a recurring timeslot
+        timeslot = RecurringTimeslot(days_of_week, start.time(), end.time(), shift_length)
+
+        timeslots = [timeslot]
+
+        # save timeslots to file
+        with open("timeslots.json", "w") as f:
+            json.dump([timeslot.to_json() for timeslot in timeslots], f)
 
     # Run once or every args.frequency seconds
     print("Starting the script...")
     while True:
         # Get shifts
         shifts = hungry.get_shifts()
-        # get timeslots
-        timeslots = [timeslot]
 
         # Get shifts that satisfy the user specified timeslots
-        valid_shifts = [shift for shift in shifts if timeslot.is_valid_shift(shift.start, shift.end)]
+        valid_shifts = set()
+        for timeslot in timeslots:
+            for shift in shifts:
+                if timeslot.is_valid_shift(shift.start, shift.end):
+                    valid_shifts.add(shift)
 
         # Notify if a valid shift is found
-        if len(shifts) > 0:
+        if len(valid_shifts) > 0:
             print(f"Found {len(shifts)} new shifts!")
             for shift in valid_shifts:
                 shifts_repr: str = '\n'.join(str(s) for s in shifts)
